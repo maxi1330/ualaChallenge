98dd7eadbe9eb37d0884aa30a0a456a14060bd07
mgnovatto <maxignov@gmail.com>
2025-07-05 16:34:41 -0300

    version database

--- app/src/main/java/com/mgnovatto/uala/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/mgnovatto/uala/data/local/db/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase
 /**
  * Abstract class that defines the Room database for the application.
  */
-@Database(entities = [CityEntity::class], version = 1, exportSchema = false)
+@Database(entities = [CityEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cityDao(): CityDao
 }
