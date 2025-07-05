e8d2fca4ef9a2d6abfcd299dce1ce7a59f4df0b4
mgnovatto <maxignov@gmail.com>
2025-07-05 16:16:49 -0300

    index database

--- app/src/main/java/com/mgnovatto/uala/data/local/db/CityEntity.kt
+++ app/src/main/java/com/mgnovatto/uala/data/local/db/CityEntity.kt
@@ -2,11 +2,15 @@ package com.mgnovatto.uala.data.local.db
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Index
 
 /**
  * Represents a city entity stored in the local Room database.
  */
-@Entity(tableName = "cities")
+@Entity(
+    tableName = "cities",
+    indices = [Index(value = ["name"])]
+)
 data class CityEntity(
     @PrimaryKey val id: Int,
     val name: String,
